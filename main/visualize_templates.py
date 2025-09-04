488de566298cb34954f552d54a6e1517aa20f97c
arata <arata.email@example.com>
2025-09-04 11:15:47 +0900

    テンプレートを可視化

--- main/visualize_templates.py
+++ main/visualize_templates.py
@@ -1,4 +1,6 @@
 import joblib
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     model = joblib.load('/home/arata/rcss/goal-scene-analyzer/data/processed/scoring_model.pkl')
@@ -8,6 +10,34 @@ def main():
     except FileNotFoundError:
         print("モデルファイルが見つかりません")
         return
+
+def visualize_template(template_data):
+    reshaped_data = template_data.reshape(50, 23, 2)
+    
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.plot([-52.5, 52.5, 52.5, -52.5, -52.5], [-34, -34, 34, 34, -34], color="gray")
+
+    num_objects = reshaped_data.shape[1]
+    colors = plt.cm.gist_rainbow(np.linspace(0, 1, num_objects))
+
+    for i in range(num_objects):
+        # i番目のオブジェクトのx座標とy座標を全時間分取り出す
+        x_coords = reshaped_data[:, i, 0]
+        y_coords = reshaped_data[:, i, 1]
+
+        if i == 0:  # ボール
+            ax.plot(x_coords, y_coords, color='black', linewidth=3, label='Ball', zorder=10)
+        else:  # 選手
+            ax.plot(x_coords, y_coords, color=colors[i], label=f'Player_{i}')
+
+    ax.set_title(os.path.basename(output_path))
+    ax.set_xlim(-55, 55)
+    ax.set_ylim(-35, 35)
+    
+    # 4. ファイルに保存
+    plt.savefig(output_path)
+    plt.close(fig) # メモリを解放するために図を閉じる
+    print(f"テンプレートを {output_path} に保存しました。")
 
 if __name__ == "__main__":
     main()
