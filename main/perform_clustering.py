afed1430ef85ae02dd3585d4a448320c15b9eccd
arata <arata.email@example.com>
2025-09-18 08:57:44 +0900

    テンプレートの割合を表示するようにする

--- main/perform_clustering.py
+++ main/perform_clustering.py
@@ -5,7 +5,7 @@ from tslearn.clustering import TimeSeriesKMeans
 def main():
     print("クラスタリング開始")
 
-    npy_path = "/home/arata/rcss/goal-scene-analyzer/data/results/scoring_scenes.npy"
+    npy_path ="/home/arata/rcss/goal-scene-analyzer/data/finish/09-11/concession_scenes.npy"
     try:
         loaded_npy = np.load(npy_path)
     except FileNotFoundError:
@@ -26,10 +26,20 @@ def main():
         return
 
     ##クラスタ数を定義する．
-    n_clusters = min(10, scene_numpy.shape[0])
+    n_clusters = min(6, scene_numpy.shape[0])
 
     model = TimeSeriesKMeans(n_clusters=n_clusters, metric="dtw", verbose=True, random_state=42, n_jobs=-1)
     model.fit(scene_numpy)
+
+    labels = model.labels_
+    cluster_ids, counts = np.unique(labels, return_counts=True)
+    sorted_indices = np.argsort(-counts)
+
+    for i in sorted_indices:
+        cluster_id = cluster_ids[i]
+        count = counts[i]
+        percentage = (count / scene_numpy.shape[0]) * 100
+        print(f"クラスタ {cluster_id}: {count} 個のシーン, 全体の {percentage:.2f}%")
 
     model_output_path = '/home/arata/rcss/goal-scene-analyzer/data/processed/scoring_model.pkl'
     joblib.dump(model, model_output_path)
