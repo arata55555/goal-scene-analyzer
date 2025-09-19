14c14d52487ad9f29e7d7e981fabd01bf1e01abc
arata <arata.email@example.com>
2025-09-19 17:13:59 +0900

    サッカーピッチを描写するように変更

--- main/visualize_templates.py
+++ main/visualize_templates.py
@@ -1,10 +1,11 @@
 import joblib
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 import numpy as np
 import os
 
 def main():
-    model = joblib.load('/home/arata/rcss/goal-scene-analyzer/data/processed/scoring_model.pkl')
+    model = joblib.load('/home/arata/rcss/goal-scene-analyzer/data/finish/09-18/scoringk=4/scoring_model.pkl')
     try:
         templates = model.cluster_centers_
         print(f"テンプレート数: {len(templates)}")
@@ -17,7 +18,7 @@ def main():
     for i, template in enumerate(templates):
         output_path = os.path.join(output_dir, f'template_{i}.png')
         visualize_template(template, output_path)
-    print("すべてのテンプレートの可視化が完了しました。")
+    print("すべてのテンプレートの可視化が完了しました")
 
 def visualize_template(template_data, output_path):
     n_timesteps = template_data.shape[0]
@@ -25,10 +26,55 @@ def visualize_template(template_data, output_path):
     reshaped_data = template_data.reshape(n_timesteps, 23, 2)
 
     fig, ax = plt.subplots(figsize=(12, 8))
-    ax.plot([-52.5, 52.5, 52.5, -52.5, -52.5], [-34, -34, 34, 34, -34], color="gray")
+
+    x_half = 52.5
+    y_half = 34.0
+    ax.plot(
+        [-x_half, x_half, x_half, -x_half, -x_half],
+        [-y_half, -y_half, y_half, y_half, -y_half],
+        color='white', linewidth=2
+    )
+
+    ax.plot([0, 0], [-y_half, y_half], color='white', linewidth=2)
+
+    ax.set_facecolor('green')
+    ax.set_aspect('equal')
+    penalty_area_right = plt.Rectangle(
+        (x_half - 16.5, -40.32 / 2), 16.5, 40.32,                         
+        ec='white',                    
+        fc='none',                     
+        lw=2                           
+        )
+    ax.add_patch(penalty_area_right) 
+    penalty_area_left = plt.Rectangle(
+        (-x_half, -40.32 / 2), 16.5, 40.32,                         
+        ec='white',                    
+        fc='none',                     
+        lw=2                           
+        )
+    ax.add_patch(penalty_area_left)
+    goal_area_right = plt.Rectangle(
+        (x_half - 5.5, -18.32 / 2), 5.5, 18.32,                         
+        ec='white',                    
+        fc='none',                     
+        lw=2                           
+        )
+    ax.add_patch(goal_area_right)
+    goal_area_left = plt.Rectangle(
+        (-x_half, -18.32 / 2), 5.5,
+        18.32,
+        ec='white',
+        fc='none',
+        lw=2
+        )
+    ax.add_patch(goal_area_left)    
+
+    center_circle = Circle((0, 0), 9.15, color='white', fill=False, linewidth=2)
+    ax.add_patch(center_circle)    
+
+    plt.show()
 
     num_objects = reshaped_data.shape[1]
-    colors = plt.cm.gist_rainbow(np.linspace(0, 1, num_objects))
 
     for i in range(num_objects):
         # i番目のオブジェクトのx座標とy座標を全時間分取り出す
@@ -38,16 +84,18 @@ def visualize_template(template_data, output_path):
         if i == 0:  # ボール
             ax.plot(x_coords, y_coords, color='black', linewidth=3, label='Ball', zorder=10)
         else:  # 選手
-            ax.plot(x_coords, y_coords, color=colors[i], label=f'Player_{i}')
+            if i <= 11:
+                ax.plot(x_coords, y_coords, color='red', label=f'Player_{i}')
+            else:
+                ax.plot(x_coords, y_coords, color='blue', label=f'Player_{i}')
 
     ax.set_title(os.path.basename(output_path))
     ax.set_xlim(-55, 55)
-    ax.set_ylim(-35, 35)
-    
-    # 4. ファイルに保存
+    ax.set_ylim(-35, 35)    
+
     plt.savefig(output_path)
-    plt.close(fig) # メモリを解放するために図を閉じる
-    print(f"テンプレートを {output_path} に保存しました。")
+    plt.close(fig) 
+    print(f"テンプレートを {output_path} に保存しました")
 
 if __name__ == "__main__":
     main()
